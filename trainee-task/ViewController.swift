c668fda6f8177e04c54972b6e69e2fe53b5e94e8
typicaldayn <krig.ua1968@gmail.com>
2022-07-07 18:53:13 +0300

    Initial Commit

---
+++ trainee-task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trainee-task
+//
+//  Created by Stas Bezhan on 07.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
